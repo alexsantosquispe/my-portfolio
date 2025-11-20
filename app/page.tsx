f78e5b1f29f91b650d217c0e776c5177d602310e
Alex Santos <alexsantosquispe@gmail.com>
2025-11-19 21:57:43 -0400

    chore: prettier-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full"></main>
+      <main className="flex min-h-screen w-full">hello world</main>
     </div>
   );
 }
